054f7e8696c233e62cf587738d1b25776a437205
developerleonardo <leo.11hnz@gmail.com>
2026-02-10 19:19:29 -0500

    feat: enhance BoardSidebar with logo and action buttons; update List component with icons; improve button styles

--- src/components/BoardSidebar.tsx
+++ src/components/BoardSidebar.tsx
@@ -3,18 +3,43 @@ import {
   SidebarContent,
   SidebarFooter,
   SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
   SidebarHeader,
 } from "@/components/ui/sidebar";
+import { Button } from "./ui/button";
+import { Search, SquarePen } from "lucide-react";
 
 export const BoardSidebar = () => {
   return (
     <Sidebar side="left">
       <SidebarHeader>
-        <h2>Boardy</h2>
+        <div className="flex gap-2 items-center">
+          <img src="./favicon.svg" alt="logo" className="w-4 h-4" />
+          <h2 className="font-bold">Boardy</h2>
+        </div>
       </SidebarHeader>
       <SidebarContent>
-        <SidebarGroup />
-        <SidebarGroup />
+        <SidebarGroup>
+          <SidebarGroupContent>
+            <Button variant="ghost" className="w-full flex justify-start">
+              <SquarePen className="mr-1" />
+              Add Board
+            </Button>
+            <Button variant="ghost" className="w-full flex justify-start">
+              <Search className="mr-1" />
+              Search Board
+            </Button>
+          </SidebarGroupContent>
+        </SidebarGroup>
+        <SidebarGroup>
+          <SidebarGroupLabel>Boards</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <Button variant="ghost" className="w-full flex justify-start">
+              My first board
+            </Button>
+          </SidebarGroupContent>
+        </SidebarGroup>
       </SidebarContent>
       <SidebarFooter />
     </Sidebar>
--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button";
+import { CirclePlus, GripVertical, Trash } from "lucide-react";
 
 type listProps = {
   title?: string;
@@ -10,45 +11,13 @@ export const List = ({ title, children }: listProps) => {
     <div className="bg-neutral-800 rounded-md flex flex-col gap-3 w-76 p-4 items-center h-fit">
       <div className="flex justify-between items-center w-full">
         <input type="text" className="grow mr-2" value={title} />
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="20"
-          height="20"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          className="lucide lucide-trash-icon lucide-trash cursor-pointer"
-        >
-          <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6" />
-          <path d="M3 6h18" />
-          <path d="M8 6V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2" />
-        </svg>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="20"
-          height="20"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          className="lucide lucide-grip-vertical-icon lucide-grip-vertical cursor-grab"
-        >
-          <circle cx="9" cy="12" r="1" />
-          <circle cx="9" cy="5" r="1" />
-          <circle cx="9" cy="19" r="1" />
-          <circle cx="15" cy="12" r="1" />
-          <circle cx="15" cy="5" r="1" />
-          <circle cx="15" cy="19" r="1" />
-        </svg>
+        <Trash className="cursor-pointer w-5 h-5" />
+        <GripVertical className="cursor-grab w-5 h-5" />
       </div>
       <div className="flex flex-col gap-2">{children}</div>
       <Button variant="secondary" className="w-full">
-        Click me
+        <CirclePlus className="mr-1" />
+        Add card
       </Button>
     </div>
   );
