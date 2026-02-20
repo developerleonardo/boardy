f5ce7a71ed9ec2baea509afa81fcacb92f39cf50
developerleonardo <leo.11hnz@gmail.com>
2026-02-20 15:59:22 -0500

    feat: add local storage persist to the project

--- src/stores/bound/bound.store.ts
+++ src/stores/bound/bound.store.ts
@@ -1,26 +1,22 @@
 import { create } from "zustand";
-//import { persist } from "zustand/middleware";
+import { persist } from "zustand/middleware";
 import { createBoardsSlice } from "../boards/boards.slice";
 import { createCardsSlice } from "../cards/cards.slice";
 import { createListsSlice } from "../lists/lists.slice";
-//import type { StateCreator } from "zustand";
+import type { StateCreator } from "zustand";
 import type { BoardsSlice } from "../boards/boards.slice";
 import type { ListsSlice } from "../lists/lists.slice";
 import type { CardsSlice } from "../cards/cards.slice";
 
 export type BoundState = BoardsSlice & ListsSlice & CardsSlice;
 
-export const useBoundStore = create<BoundState>((...args) => ({
+const createBoundStore: StateCreator<BoundState> = (...args) => ({
   ...createBoardsSlice(...args),
   ...createListsSlice(...args),
   ...createCardsSlice(...args),
-}));
-
-// const createBoundStore: StateCreator<BoundState> = (...args) => ({
-//   ...createBoardsSlice(...args),
-//   ...createListsSlice(...args),
-//   ...createCardsSlice(...args),
-// });
-// export const useBoundStore = create(
-//   persist(createBoundStore, { name: "boardy-storage" }),
-// );
+});
+export const useBoundStore = create(
+  persist(createBoundStore, {
+    name: "kanban-storage",
+  }),
+);
